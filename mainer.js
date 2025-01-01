d5d27f71af7d6c15fc449f83ff602cd200d274f4
Ayman-brr <114749944+Ayman-brr@users.noreply.github.com>
2025-01-01 20:09:42 +0600

    Update mainer.js

--- mainer.js
+++ mainer.js
@@ -45,7 +45,7 @@ const pins = [
     { id: 44, imageUrl: './images/zabid_gelo_palay.jpg', title: 'zabid_gelo_palay', description: '' },
     { id: 45, imageUrl: './images/zabid_not_lit.jpg', title: 'zabid_not_lit', description: '' },
     { id: 46, imageUrl: './images/zabid_own_bully.jpg', title: 'zabid_own_bully', description: '' },
-    { id: 47, imageUrl: './images/atif_👍.jpg', title: 'Atif 👍', description: '👍' },
+    { id: 47, imageUrl: './images/atif_happy.jpg', title: 'Atif 👍', description: '👍' },
 ];
 
 const CONFIG = {
